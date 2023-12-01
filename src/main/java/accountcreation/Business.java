09e00994e8d198e784bac043406ffa4890c60a35
flute321 <suchitastargirl@gmail.com>
2023-12-01 16:42:42 -0500

    message

--- src/main/java/accountcreation/Business.java
+++ src/main/java/accountcreation/Business.java
@@ -31,13 +31,5 @@ public class Business {
         return dataBase;
     }
 
-    //to be implemented
-    public void sendApprovalEmail(DataEntry immigrant) {
-        
-    }
-
-    public void sendDenialEmail(DataEntry immigrant) {
-        
-    }
     
 }
--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -4,11 +4,10 @@ import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
+import CS321.src.main.Workflow;
 import CS321.src.main.Approval;
 import CS321.src.main.Business;
 import CS321.src.main.DataEntry;
-import CS321.src.main.Workflow;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
@@ -19,11 +18,25 @@ public class Tester
 {
 
     ////////////////////////////////////////////////////////////////////////////
-    //Data Entry test functions
+    //testing Workflow class
+   @Test
+    public void checkWorkflow() {
 
+        Workflow wf = new Workflow();
+        assertTrue(wf.getTable() != null);
+    }
+
+
+
+    /////////////////////////////////////////////////
+    //Business Object Test cases
+  
     @Test
-    public void testValidateText() {
-        DataEntry dataEntry = new DataEntry(null, null);
+    public void checkAddDB() {
+
+        Business obj = new Business();
+        DataEntry dataEntry = new DataEntry(null, obj);
+
 
         // Valid input
         dataEntry.setFirstName("John");
@@ -33,77 +46,46 @@ public class Tester
         dataEntry.setState("State");
         dataEntry.setJob("Job");
         dataEntry.setEducation("Education");
-        assertTrue(dataEntry.validateText());
+        dataEntry.setPhoneNumber("12345678901");
+        dataEntry.setDob(20201231); 
+        dataEntry.setPhoneNumber("1234567890"); 
 
-        // Invalid input
-        dataEntry.setFirstName("John123"); // Contains digits
-        assertFalse(dataEntry.validateText());
+        obj.add_DB(dataEntry);
+        assertTrue(obj.getSize() == 0);
 
     }
+
 
     @Test
-    public void testValidateNumbers() {
-        DataEntry dataEntry = new DataEntry(null, null);
+    public void checkRetrieveDB() {
+
+        Business obj = new Business();
+        DataEntry dataEntry = new DataEntry(null, obj);
+
 
         // Valid input
-        dataEntry.setPhoneNumber("12345678901"); // 11 digits
-        dataEntry.setDob(20201231); // 8 digits
-        assertTrue(dataEntry.validateNumbers());
+        dataEntry.setFirstName("John");
+        dataEntry.setLastName("Doe");
+        dataEntry.setEmail("john.doe@example.com");
+        dataEntry.setCity("City");
+        dataEntry.setState("State");
+        dataEntry.setJob("Job");
+        dataEntry.setEducation("Education");
+        dataEntry.setPhoneNumber("12345678901");
+        dataEntry.setDob(20201231); 
+        dataEntry.setPhoneNumber("1234567890"); 
 
-        // Invalid input
-        dataEntry.setPhoneNumber("1234567890"); // Less than 11 digits
-        assertFalse(dataEntry.validateNumbers());
-
-    }
-
-    @Test
-    public void testValidateNumEntries() {
-        DataEntry dataEntry = new DataEntry(null, null);
-
-        // Valid input
-        assertTrue(dataEntry.validateNumEntries("12345")); // Valid integer
-
-        // Invalid input
-        assertFalse(dataEntry.validateNumEntries("abc")); // Not a valid integer
+        obj.add_DB(dataEntry);
+        assertTrue(obj.retrieve_DB(0).getFirstName()  == "John");
 
     }
-
-
-    
-   
-    ////////////////////////////////////////////////////////////////////////////
-    //testing workflow class
-
-
-     
-   
-
-    /////////////////////////////////////////////////
-    //Approval Test cases
-
-
-
-     
-   
-
-   
 
 
     public static void main(String[] args)
     {
-        testDeny();
-        testApprove();
-        testClearScreen();
-        testDisplayScreen();
-        testGetAlienNumber();
-        testGetCurrentStep();
-        testGetImmigrant();
-        testGetNext();
-        testHasNext();
-        testSetAlienNumber();
-        testSetCurrentStep();
-        testSetImmigrant();
-        
+       checkAddDB();
+       checkRetrieveDB();
+       checkWorkflow();
 
     }
 
