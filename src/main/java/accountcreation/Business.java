58c8ee773de4935df6d322665f76c17b604fbe7a
flute321 <suchitastargirl@gmail.com>
2023-12-01 14:33:12 -0500

    adding files

---
+++ src/main/java/accountcreation/Business.java
@@ -0,0 +1,43 @@
+package CS321.src.main;
+import java.util.*;
+import java.io.*;
+
+public class Business {
+
+    private ArrayList<DataEntry> dataBase;
+
+    //iniatizes list containing immigrants
+    public Business(){
+        dataBase = new ArrayList<DataEntry>();
+    }
+
+    public void add_DB(DataEntry immigrant) {
+        dataBase.add(immigrant);
+    }
+
+    public DataEntry retrieve_DB(int index) {
+        return dataBase.get(index);
+    }
+
+    //get the size of the table
+    public int getSize()
+    {
+        return dataBase.size();
+    }
+
+    //get the table
+    public ArrayList<DataEntry> getTable()
+    {
+        return dataBase;
+    }
+
+    //to be implemented
+    public void sendApprovalEmail(DataEntry immigrant) {
+        
+    }
+
+    public void sendDenialEmail(DataEntry immigrant) {
+        
+    }
+    
+}
---
+++ src/main/java/accountcreation/Workflow.java
@@ -0,0 +1,73 @@
+package CS321.src.main;
+import java.util.ArrayList;
+
+
+public class Workflow {
+
+    //create a table variable
+    ArrayList<Integer> table;
+
+    //constructor to initialze the table
+    public Workflow()
+    {
+        table = new ArrayList<>();
+    }
+
+    //get the size of the table
+    public int getSize()
+    {
+        return table.size();
+    }
+
+    //get the table
+    public ArrayList<Integer> getTable()
+    {
+        return table;
+    }
+
+    //main method
+    public static void main(String[] args)
+    {
+        //create a workflow table
+        Workflow immigrantTable = new Workflow();
+       
+        //create a business object
+        Business business = new Business();
+        
+        //create a temporary DataEntry variable
+        DataEntry temp = new DataEntry(immigrantTable, business);
+
+        //do while loop to keep going until the user presses quit
+        while(!temp.getQuit())
+        {
+            if(temp.getSubmit())
+            {
+                temp = new DataEntry(immigrantTable, business);
+            }
+        }
+
+        //create an approval object
+        Approval approval = new Approval(business, immigrantTable.getTable().get(0));
+
+        //if there is another business object
+        while(immigrantTable.getTable().size() > 0)
+        {
+
+            //if the user submitted whether or not they accepted or rejected the applicant
+            if(approval.canNext())
+            {
+                //remove the top item to move through the workflow
+                immigrantTable.getTable().remove(0);
+                
+                if(immigrantTable.getTable().size() == 0)
+                    break;
+                
+                approval = new Approval(business, immigrantTable.getTable().get(0));
+            }
+
+            
+        }
+
+    }
+
+}
