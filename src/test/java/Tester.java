d55632575b09e69d8f508b81b742bea173729a8b
flute321 <suchitastargirl@gmail.com>
2023-12-01 14:49:38 -0500

    trying unit test case formats

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -21,172 +21,72 @@ public class Tester
     ////////////////////////////////////////////////////////////////////////////
     //Data Entry test functions
 
+    @Test
+    public void testValidateText() {
+        DataEntry dataEntry = new DataEntry(null, null);
 
- @Test
-    public static void testDisplay() {
-        try {
-          DataEntry obj = new DataEntry();
-          assertTrue(true, obj.displayScreen());
+        // Valid input
+        dataEntry.setFirstName("John");
+        dataEntry.setLastName("Doe");
+        dataEntry.setEmail("john.doe@example.com");
+        dataEntry.setCity("City");
+        dataEntry.setState("State");
+        dataEntry.setJob("Job");
+        dataEntry.setEducation("Education");
+        assertTrue(dataEntry.validateText());
 
-        } 
-        
-        catch (Exception e) {
-          System.out.println("display screen");
-        }
+        // Invalid input
+        dataEntry.setFirstName("John123"); // Contains digits
+        assertFalse(dataEntry.validateText());
+
     }
-
 
     @Test
-    public static void testSubmit() {
-        try {
-          DataEntry obj = new DataEntry();
-          assertTrue(true, obj.submit());
+    public void testValidateNumbers() {
+        DataEntry dataEntry = new DataEntry(null, null);
 
-        } 
-        
-        catch (Exception e) {
-          System.out.println("display screen");
-        }
+        // Valid input
+        dataEntry.setPhoneNumber("12345678901"); // 11 digits
+        dataEntry.setDob(20201231); // 8 digits
+        assertTrue(dataEntry.validateNumbers());
+
+        // Invalid input
+        dataEntry.setPhoneNumber("1234567890"); // Less than 11 digits
+        assertFalse(dataEntry.validateNumbers());
+
+    }
+
+    @Test
+    public void testValidateNumEntries() {
+        DataEntry dataEntry = new DataEntry(null, null);
+
+        // Valid input
+        assertTrue(dataEntry.validateNumEntries("12345")); // Valid integer
+
+        // Invalid input
+        assertFalse(dataEntry.validateNumEntries("abc")); // Not a valid integer
+
     }
 
 
+    
    
     ////////////////////////////////////////////////////////////////////////////
     //testing workflow class
 
 
-    @Test
-    public static void testGetNext() {
-        try {
-          Workflow workflow = new Workflow();
-          int nextStep = workflow.getNext(2);
-          assertEquals(3, nextStep);
-        } 
-        catch (AssertionError e) {
-          System.out.println("next step");
-        }
-    }
-
-    @Test
-    public static void testHasNext() {
-        try {
-          Workflow workflow = new Workflow();
-          boolean hasNext = workflow.hasNext(2);
-          assertTrue(hasNext);
-        } 
-
-        catch (AssertionError e) {
-          System.out.println("has a next?");
-        }
-    }
+     
+   
 
     /////////////////////////////////////////////////
     //Approval Test cases
 
 
 
-    @Test
-    public static void testApprove() {
-        try {
-          Approval approval = new Approval();
-          boolean result = approval.approve(12345); 
-          assertTrue(result);
-        } 
-        
-        catch (AssertionError e) {
-          System.out.println("approve method");
-        }
-    }
+     
+   
 
-    @Test
-    public static void testDisplayScreen() {
-        try {
-          Approval approval = new Approval();
-          assertTrue(true, approval.displayScreen());
-
-        } 
-        
-        catch (Exception e) {
-          System.out.println("display screen");
-        }
-    }
-
-    @Test
-    public static void testClearScreen() {
-        try {
-          Approval approval = new Approval();
-          assertTrue(true, approval.clearScreen());
-        } 
-        
-        catch (AssertionError e) {
-          System.out.println("Exception occurred: " + e.getMessage());
-        }
-    }
-
-    @Test
-    public static void testDeny(){
-        try {
-          Approval approval = new Approval();
-          boolean result = approval.deny(12345); 
-          assertFalse(result);
-        } 
-        
-        catch (AssertionError e) {
-          System.out.println("test deny");
-        }
-    }
-
-    /*
-     * @author Sadhana Mallemudi
-     */
-    @Test
-    @DisplayName("Test Immigrant")
-    public static void testImmigrant(){
-      Business immigrant = new Business();
-      Assertions.assertNotNull(immigrant);
-    }
-    @Test
-    @DisplayName("Test Tracking Number")
-    public static void testGenerateTrackingNum(){
-      Business immigrant = new Business();
-      int num = immigrant.generateTrackingNumber(immigrant);
-      Assertions.assertEquals(0, num);
-    }
-    @Test
-    @DisplayName("Test if it's added to database")
-    public static void testDatabaseAdded(){
-      Business immigrant = new Business();
-      boolean check = immigrant.addToDatabase(immigrant);
-      Assertions.assertEquals(false, check);
-    }
-    @Test
-    @DisplayName("Test if able to retrieve from database")
-    public static void testRetrieveFromDatabase(){
-      Business immigrant = new Business();
-      boolean check = immigrant.retrieveFromDatabase(immigrant);
-      Assertions.assertEquals(false, check);
-    }
-    @Test
-    @DisplayName("Test for approval status")
-    public static void testApprovalStatus(){
-      Business immigrant = new Business();
-      boolean check = immigrant.getApprovalStatus(immigrant);
-      Assertions.assertEquals(false, check);
-    }
-    @Test
-    @DisplayName("Test for approval email")
-    public static void testApprovalEmail(){
-      Business immigrant = new Business();
-      boolean check = immigrant.sendApprovalEmail(immigrant);
-      Assertions.assertEquals(false, check);
-    }
-    @Test
-    @DisplayName("Test for denial email")
-    public static void testDenialEmail(){
-      Business immigrant = new Business();
-      boolean check = immigrant.sendDenialEmail(immigrant);
-      Assertions.assertEquals(false, check);
-    }
+   
 
 
     public static void main(String[] args)
